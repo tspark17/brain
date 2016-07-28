07a010c9ccf66f9180a32fe4700a566162d93c12
Bean <Bean@hex02.local>
2016-07-28 09:48:57 +0900

    7/28 9:48

--- TestWindowBuilder/src/TestWB.java
+++ TestWindowBuilder/src/TestWB.java
@@ -15,10 +15,37 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JLabel;
 import java.awt.Color;
 import javax.swing.ImageIcon;
+import javax.swing.JScrollPane;
+import java.awt.Panel;
+import javax.swing.JLayeredPane;
+import javax.swing.JSpinner;
+import javax.swing.JToggleButton;
+import javax.swing.JScrollBar;
+import javax.swing.JList;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ButtonGroup;
+import java.awt.Button;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.border.LineBorder;
 
 public class TestWB extends JFrame {
-
+//https://www.youtube.com/watch?v=xkcs25Ustag
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private final Action action = new SwingAction();
+	private final Action action_1 = new SwingAction_1();
+	private final Action action_2 = new SwingAction_2();
+	private final Action action_3 = new SwingAction_3();
+	private final Action action_4 = new SwingAction_4();
 
 	/**
 	 * Launch the application.
@@ -41,48 +68,125 @@ public class TestWB extends JFrame {
 	 */
 	public TestWB() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 561, 497);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
 		setContentPane(contentPane);
 		
-		JButton btnTest = new JButton("test");
-		
-		JButton btnTest_1 = new JButton("test2");
-		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Kyeol\\Desktop\\java.png"));
-		lblNewLabel.setForeground(Color.BLUE);
-		lblNewLabel.setBackground(Color.WHITE);
+		JPanel panel = new JPanel();
+		panel.setToolTipText("");
+		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setAlignOnBaseline(true);
+		flowLayout.setAlignment(FlowLayout.LEFT);
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(75)
-							.addComponent(btnTest, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
-							.addGap(67)
-							.addComponent(btnTest_1))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(36)
-							.addComponent(lblNewLabel)))
-					.addContainerGap(49, Short.MAX_VALUE))
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 223, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(312, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 113, Short.MAX_VALUE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-							.addComponent(btnTest_1, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)
-							.addGap(20))
-						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
-							.addComponent(btnTest, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
-							.addGap(55))))
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addGap(144)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 154, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(150, Short.MAX_VALUE))
 		);
+		
+		JButton geoFileLoad = new JButton("Geometry File");
+		geoFileLoad.setAction(action);
+		geoFileLoad.setText("Geometry File");
+		geoFileLoad.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel.add(geoFileLoad);
+		
+		textField = new JTextField();
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnTissueFile = new JButton("Tissue File");
+		btnTissueFile.setAction(action_1);
+		geoFileLoad.setText("Tissue File");
+		btnTissueFile.setEnabled(false);
+		btnTissueFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		panel.add(btnTissueFile);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		panel.add(textField_1);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setAction(action_2);
+		btnNewButton.setEnabled(false);
+		panel.add(btnNewButton);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		panel.add(textField_2);
+		
+		JButton button_1 = new JButton("New button");
+		button_1.setAction(action_3);
+		button_1.setEnabled(false);
+		panel.add(button_1);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		panel.add(textField_3);
+		
+		JButton button_2 = new JButton("New button");
+		button_2.setAction(action_4);
+		button_2.setEnabled(false);
+		panel.add(button_2);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		panel.add(textField_4);
 		contentPane.setLayout(gl_contentPane);
+	}
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_1 extends AbstractAction {
+		public SwingAction_1() {
+			putValue(NAME, "SwingAction_1");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_2 extends AbstractAction {
+		public SwingAction_2() {
+			putValue(NAME, "SwingAction_2");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_3 extends AbstractAction {
+		public SwingAction_3() {
+			putValue(NAME, "SwingAction_3");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+	private class SwingAction_4 extends AbstractAction {
+		public SwingAction_4() {
+			putValue(NAME, "SwingAction_4");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
 	}
 }
