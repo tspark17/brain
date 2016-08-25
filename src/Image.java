6b3d63aaa5ed82f6393a154608ae6336f42ac1cd
Kyeol <Kyeol@DESKTOP-2UB3TFP>
2016-08-25 13:52:06 +0900

    8/25 image.java convert

--- src/Image.java
+++ src/Image.java
@@ -1,4 +1,79 @@
+package string_image;
 
-public class Image {
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
+import javax.imageio.ImageIO;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public class ImageUtils {
+
+	/**
+	 * Decode string to image
+	 * 
+	 * @param imageString
+	 *            The string to decode
+	 * @return decoded image
+	 */
+	public static BufferedImage decodeToImage(String imageString) {
+
+		BufferedImage image = null;
+		byte[] imageByte;
+		try {
+			BASE64Decoder decoder = new BASE64Decoder();
+			imageByte = decoder.decodeBuffer(imageString);
+			ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
+			image = ImageIO.read(bis);
+			bis.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return image;
+	}
+
+	/**
+	 * Encode image to string
+	 * 
+	 * @param image
+	 *            The image to encode
+	 * @param typed
+	 *            jpeg, bmp, ...
+	 * @return encoded string
+	 */
+	public static String encodeToString(BufferedImage image, String type) {
+		String imageString = null;
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+		try {
+			ImageIO.write(image, type, bos);
+			byte[] imageBytes = bos.toByteArray();
+
+			BASE64Encoder encoder = new BASE64Encoder();
+			imageString = encoder.encode(imageBytes);
+
+			bos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return imageString;
+	}
+
+	public static void main(String args[]) throws IOException {
+		/* Test image to string and string to image start */
+		String imgstr = new Scanner(new File("C:/Users/Kyeol/Desktop/MRSim_Geometry_7T_xy98_Quad_WithHead.txt")).useDelimiter("\\A").next();
+		//System.out.println(entireFileText);
+		
+		BufferedImage newImg; 
+		System.out.println(imgstr);
+		newImg = decodeToImage(imgstr);
+		ImageIO.write(newImg, "png", new File("C:/Users/Kyeol/Desktop/MRSim_Geometry_7T_xy98_Quad_WithHead.png"));
+		/* Test image to string and string to image finish */
+	}
 }
+
