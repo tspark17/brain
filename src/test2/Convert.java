6905fc9cc22ede8460b510f7fee3dc9bc027a858
taeshin <taeshin@tspark.local>
2016-09-07 16:32:40 +0900

    0907 4:30 Part 수정2

--- src/test2/Convert.java
+++ src/test2/Convert.java
@@ -24,7 +24,7 @@ public class Convert extends HttpServlet{
 		// TODO Auto-generated method stub
 		//File file = (File) req.getParameter("geo_load");
 		//String des = req.getParameter("geo_load");
-		//req.setCharacterEncoding("EUC-KR");
+		//req.setCharacterEncoding("EUC-KR");  한글 파일명 깨짐에 대한 설정 -> Window-preferences 에서 설정.
 		
 		Part geo_part = req.getPart("geo_load");
 		Part tissue_part = req.getPart("tissue_load");
@@ -32,30 +32,22 @@ public class Convert extends HttpServlet{
 		Part b1p_part = req.getPart("b1p_load");
 		Part b1m_part = req.getPart("b1m_load");
 		Part filePart = null;
-		for(Part part : req.getParts())
+		
+		for(Part part : req.getParts())  //form:file 형식에서 들어온 file 검사
 		{
-			System.out.println(part);
-			for(String headerName : part.getHeaderNames())
-				System.out.println("header Name:"+ headerName + "-" + part.getHeaderNames());
-			
+			if(part.getSize() != 0)  //어떤  input에서 온 file인지 검사
+			{
+				System.out.println("inputName:" + part.getName());
+				System.out.println("fileName:" + getFileName(part));
+				System.out.println("fileSize:" + part.getSize());
+			}
 		}
 
-		String fileName = getFileName(geo_part);
 		//String fileName = Paths.get(part.getSubmittedFileName()).getFileName().toString();
 		InputStream fileContent = geo_part.getInputStream();
-		
-		if(geo_part.getSize() != 0)
-			System.out.println("geo ON");
-		else if(tissue_part.getSize() != 0)
-			System.out.println("tissue ON");
-		else if(b0_part.getSize() != 0)
-			System.out.println("b0 ON");
-		else if(b1p_part.getSize() != 0)
-			System.out.println("b1p ON");
-		else if(b1m_part.getSize() != 0)
-			System.out.println("b1m ON");
-//		System.out.println("part:" + geo_part);
-	//	System.out.println("part name:" + geo_part.getName());
+
+		//System.out.println("part:" + geo_part);
+		//System.out.println("part name:" + geo_part.getName());
 		//System.out.println("part size:" + geo_part.getSize());
 		//System.out.println("part inputStream:" + geo_part.getInputStream());
 		//System.out.println("des:" + des);
