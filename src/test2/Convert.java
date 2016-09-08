c02bf04d339eaaa3587bb0dcb18b5923b111455d
taeshin <taeshin@tspark.local>
2016-09-08 10:37:14 +0900

    0908 10:36 part로부터 받은 file 내용 읽는 function추가.(readFile, getStringFromStream)

--- src/test2/Convert.java
+++ src/test2/Convert.java
@@ -1,8 +1,8 @@
 package test2;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Paths;
-import java.util.Enumeration;
+import java.io.InputStreamReader;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -13,6 +13,9 @@ import javax.servlet.http.Part;
 
 //import org.apache.log4j.Logger;
 
+/**
+ * Main.jsp의 form-file에서 bin파일을 변환하는 class
+ */
 @MultipartConfig(maxFileSize=1024*1024*1) // 파일 크기 설정  - 1mb
 @SuppressWarnings("serial")
 public class Convert extends HttpServlet{
@@ -22,29 +25,33 @@ public class Convert extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//File file = (File) req.getParameter("geo_load");
-		//String des = req.getParameter("geo_load");
+
 		//req.setCharacterEncoding("EUC-KR");  한글 파일명 깨짐에 대한 설정 -> Window-preferences 에서 설정.
-		
-		Part geo_part = req.getPart("geo_load");
-		Part tissue_part = req.getPart("tissue_load");
-		Part b0_part = req.getPart("b0_load");
-		Part b1p_part = req.getPart("b1p_load");
-		Part b1m_part = req.getPart("b1m_load");
-		Part filePart = null;
-		
-		for(Part part : req.getParts())  //form:file 형식에서 들어온 file 검사
+		String inputName;
+		for(Part part : req.getParts())  //form-input-file 에 대한 각각의 항목 검사
 		{
-			if(part.getSize() != 0)  //어떤  input에서 온 file인지 검사
+			readFile(part.getInputStream());
+			if(part.getSize() != 0)  //항목마다 파일이 있는지 검사
 			{
+				//bin 파일 정보
 				System.out.println("inputName:" + part.getName());
 				System.out.println("fileName:" + getFileName(part));
 				System.out.println("fileSize:" + part.getSize());
+
+				inputName = part.getName();
+
+				//항목에 따른 파일 변환 함수 call
+				if(inputName.equals("geo_load"))
+					System.out.println("geo");
+				else if(inputName.equals("tissue_load"))
+					System.out.println("tissue");
+				else
+					System.out.println("error");
 			}
 		}
 
 		//String fileName = Paths.get(part.getSubmittedFileName()).getFileName().toString();
-		InputStream fileContent = geo_part.getInputStream();
+		//InputStream fileContent = geo_part.getInputStream();
 
 		//System.out.println("part:" + geo_part);
 		//System.out.println("part name:" + geo_part.getName());
@@ -56,24 +63,63 @@ public class Convert extends HttpServlet{
 		//System.out.println("fileName: " + fileName);
 		//System.out.println("read: " + fileContent.read());
 		//log.debug("Convert class - Begin" + test);
-		
+
 		//resp.sendRedirect("/test2/NewFile.jsp");  
-		req.getRequestDispatcher("Main.jsp");
-	
+		req.getRequestDispatcher("/test2/NewFile.jsp");
+
 		// log.debug("Convert class - End");
 	}
-	
-	//JSP로부터 요청받은 파일명을 리턴한다.
+
+	/**
+	 *bin 파일명을 return한다 
+	 */
 	String getFileName(Part part){
 		String fileName = null;        
 		String contentDispositionHeader = part.getHeader("content-disposition");
 		String [] elements = contentDispositionHeader.split(";");
 
-	        for(String element: elements)
-	        {
-	            fileName = element.substring(element.indexOf('=')+1);
-	            fileName = fileName.trim().replace("\""," ");
-	        }        
+		for(String element: elements)
+		{
+			fileName = element.substring(element.indexOf('=')+1);
+			fileName = fileName.trim().replace("\""," ");
+		}        
 		return fileName;
 	}
+	
+	/**
+	 * file을 getStringFromStream 함수로부터 읽는다. 
+	 * @param stream
+	 * @throws IOException
+	 */
+	void readFile(InputStream stream) throws IOException{
+		String file = getStringFromStream(stream);
+		System.out.println(file);
+	}
+	
+	/**
+	 * 요청한 file을 string형태로 변환한다.
+	 * bin파일, 글자 깨짐-변환 필요
+	 * @return file data
+	 * @throws IOException
+	 */
+	String getStringFromStream(InputStream stream)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(stream, "UTF-8")); 
+		StringBuilder sb = new StringBuilder(); 
+		String line = null; 
+		try { 
+			while ((line = br.readLine()) != null) { 
+				sb.append(line + "\n"); 
+			} 
+		} catch (IOException e) { 
+			e.printStackTrace(); 
+		} finally { 
+			try { 
+				stream.close(); 
+			} catch (IOException e) { 
+				e.printStackTrace(); 
+			} 
+		} 
+		return sb.toString(); 
+	} 
 }
+
